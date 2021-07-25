3e771b0e6f19c465cd2b54f9ecd6070b74ff8f98
Enchan <enchant0130@gmail.com>
2021-07-26 08:03:52 +0900

    [Modify #7] add member 'pref' into Property

--- vcflib/properties/Property.py
+++ vcflib/properties/Property.py
@@ -5,7 +5,8 @@ from typing import Dict, List, Union
 
 class Property():
 
-    def __init__(self, name: str = "", parameters: Dict[str, Union[str, List[str]]] = {}, value: bytes = bytes()) -> None:
+    def __init__(self, name: str = "", parameters: Dict[str, Union[str, List[str]]] = {}, value: bytes = bytes(), pref: bool = False) -> None:
         self.name: str = name
         self.parameters: Dict[str, Union[str, List[str]]] = parameters
         self.value: bytes = value
+        self.pref: bool = pref
